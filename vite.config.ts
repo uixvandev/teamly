671b86062540f152f6cfac31bf18e66c54ac3771
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2025-09-10 01:25:35 +0700

    Update vite config for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/teamly/",
+  base: "/",
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
